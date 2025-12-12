70240d7774a8bedbab256e3d8d670b6812e4bfb1
Maximilian Newman Loussouarn <maximilian.newman@icloud.com>
2025-12-12 14:41:31 -0500

    Fixed replay attack vulnerability

--- py dry run.py
+++ py dry run.py
@@ -1,69 +1,101 @@
 import hashlib
 import time
 import socket
+import random
+import time
 
 
-def sign(message):
+def generate_salt():
+    salt = ""
+    for i in range(30):
+        salt += random.choice("QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890")
+        salt += str(time.time)
+    return salt
+
+lastMsgNum = 0
+
+def sign(message, msgNum):
     message += "secret password 1234abcd"
+    message += salt + str(msgNum)
     message += "\0"
     sha256_hash = hashlib.sha256(message.encode("utf-8"))
     return str(sha256_hash.hexdigest())
 
 
-connected = True
+salt = generate_salt()
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(("192.168.4.1", 8080))
-        while connected:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(("192.168.4.1", 8080))
+    s.sendall(b"salt=\n" + salt.encode("utf-8") + b"\n")
+    while True:
+        protocol = ""
+        while protocol.count("\n") == 0:
+            protocol += s.recv(1).decode("utf-8")
+        protocol = protocol[:-1]
+
+        
+        if protocol == "remote-v0.1" or protocol == "remote-v0.2":
+            print("Please update your remote firmware")
+            print("It is using an old version of the communication protocol.")
+            print("Support for the old protocol was dropped due to vulnerability to replay attacks")
+            print()
+        
+        elif protocol == "remote-v1.0":
+            mLength = int(s.recv(10).decode("utf-8"))
             msg = ""
-            while msg.count("\n") < 3:
-                msg += s.recv(4096).decode("utf-8")
+            while len(msg) < mLength:
+                msg += s.recv(mLength - len(msg)).decode("utf-8")
 
             msg = msg.split("\n")
             
-            if msg[0] == "remote-v0.1" or msg[0] == "remote-v0.2":
-                instruction = msg[1]
-                verifyHash = sign(instruction)
-
-                if verifyHash == msg[2]:
-
-                    if instruction == "space press":
-                        print("pressed space")
-                    elif instruction == "space release":
-                        print("released space")
-
-                    elif instruction == "right press":
-                        print("pressed right click")
-                    elif instruction == "right release":
-                        print("released right click")
-
-                    elif instruction == "left press":
-                        print("pressed left click")
-                    elif instruction == "left release":
-                        print("released left click")
-
-                    elif instruction.startswith("mouse:"):
-                        instruction = instruction.split(":")
-                        pos = instruction[1].split(",")
-                        vx = pos[0]
-                        vy = pos[1]
-                        print("moveed mouse x", vx, "y", vy)
-
-                    else:
-                        print("unknown unstruction:")
-                        print(instruction)
-
-                    if msg[0] == "remote-v0.2":
-                        s.sendall(b"OK")
-                    
-                else:
-                    print("Failed signature check!")
+            instruction = msg[0]
+            msgNum = int(msg[1])
+            recvSignature = msg[2]
             
+            verifyHash = sign(instruction, msgNum)
+
+            if verifyHash == recvSignature and msgNum > lastMsgNum:
+                lastMsgNum = msgNum
+
+                if instruction == "space press":
+                    print("pressed space")
+                elif instruction == "space release":
+                    print("released space")
+
+                elif instruction == "right press":
+                    print("pressed right click")
+                elif instruction == "right release":
+                    print("released right click")
+
+                elif instruction == "left press":
+                    print("pressed left click")
+                elif instruction == "left release":
+                    print("released left click")
+
+                elif instruction.startswith("mouse:"):
+                    instruction = instruction.split(":")
+                    pos = instruction[1].split(",")
+                    vx = pos[0]
+                    vy = pos[1]
+                    print("moved mouse x", vx, "y", vy)
+
+                else:
+                    print("unknown unstruction:")
+                    print(instruction)
+
+                s.sendall(b"OK\n")
+                
+            else:
+                print("Failed signature check!")
+                s.sendall(b"salt=\n" + salt.encode("utf-8") + b"\n")
+
+        else:
+            print("unknown protocol:", protocol)
+        
 
 
 
 
 
-    print("Disconnected, attempting to reconnect...")
-    time.sleep(5)
+print("Disconnected, attempting to reconnect...")
+time.sleep(5)
